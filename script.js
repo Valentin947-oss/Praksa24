11317ac439cb9be541f255f9a2ac49bc14b797b2
Valentin Stojanovski-INKI947 <121190640+Valentin947-oss@users.noreply.github.com>
2024-09-23 13:30:28 +0200

    js connections

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const signUpForm=document.getElementById('signUp');
 signUpButton.addEventListener('click',function(){
     signInForm.style.display="none";
     signUpForm.style.display="block";
-})
+});
 signInButton.addEventListener('click', function(){
     signInForm.style.display="block";
     signUpForm.style.display="none";
-})
+});
